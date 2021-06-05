2af4022fbff60b62ee5eda8fade98864472aa0d6
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-06-06 01:00:56 +0200

    Fix issue with the adjacency matrix temporarily

--- src/models/Matrix/Matrix.ts
+++ src/models/Matrix/Matrix.ts
@@ -1,22 +1,28 @@
 abstract class Matrix<RowType, ColumnType, ValueType> {
 	defaultValue: ValueType;
-	matrix: Map<RowType, Map<ColumnType, ValueType>> = new Map();
+	// ToDo: Make own map which takes in an equality argument
+	matrix: Map<string, Map<string, ValueType>> = new Map();
 
 	constructor(defaultValue: ValueType) {
 		this.defaultValue = defaultValue;
 	}
 
 	set(row: RowType, column: ColumnType, value: ValueType): void {
-		if (!this.matrix.has(row)) this.matrix.set(row, new Map());
+		const rowJson = JSON.stringify(row);
+		const columnJson = JSON.stringify(column);
+		if (!this.matrix.has(rowJson)) this.matrix.set(rowJson, new Map());
 
-		this.matrix.get(row)!.set(column, value);
+		this.matrix.get(rowJson)!.set(columnJson, value);
 	}
 
 	get(row: RowType, column: ColumnType): ValueType {
-		const rowData = this.matrix.get(row);
+		const rowJson = JSON.stringify(row);
+		const columnJson = JSON.stringify(column);
+
+		const rowData = this.matrix.get(rowJson);
 		if (rowData === undefined) return this.defaultValue;
 
-		const cellData = rowData.get(column);
+		const cellData = rowData.get(columnJson);
 		if (cellData === undefined) return this.defaultValue;
 
 		return cellData;
