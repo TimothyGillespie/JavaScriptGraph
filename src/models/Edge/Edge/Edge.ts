8fd59c762c679eed87e1872807099dc2fa0f33ee
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-06-05 15:52:07 +0200

    Add equals and compareTo method for Edge

--- src/models/Edge/Edge/Edge.ts
+++ src/models/Edge/Edge/Edge.ts
@@ -9,6 +9,17 @@ abstract class Edge<V extends Vertex> {
 		this.vertexB = vertexB;
 	}
 
+	equals(other: Edge<V>): boolean {
+		return this.vertexA.equals(other.vertexA) && this.vertexB.equals(other.vertexB);
+	}
+
+	compareTo(other: this): number {
+		const aCompare = this.vertexA.compareTo(other.vertexA);
+		if (aCompare !== 0) return aCompare;
+
+		return this.vertexB.compareTo(other.vertexB);
+	}
+
 	abstract isDirected(): boolean;
 }
 
