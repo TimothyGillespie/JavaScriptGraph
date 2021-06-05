e9a93056313c70c63f940f5297b5403ffaff8828
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-06-05 19:56:28 +0200

    Implement equals of UndirectedEdge.ts appropiately

--- src/models/Edge/UndirectedEdge/UndirectedEdge.ts
+++ src/models/Edge/UndirectedEdge/UndirectedEdge.ts
@@ -5,6 +5,12 @@ class UndirectedEdge<V extends Vertex> extends Edge<V> {
 	isDirected(): boolean {
 		return false;
 	}
+
+	equals(other: Edge<V>): boolean {
+		if (other.isDirected()) return false;
+
+		return super.equals(other) || (this.vertexA.equals(other.vertexB) && this.vertexB.equals(other.vertexA));
+	}
 }
 
 export default UndirectedEdge;
