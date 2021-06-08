06c681cdd1233c95823aa0a87a8f8d140a45fc15
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-06-08 20:58:51 +0200

    Add GDF definitions for petersonGraph

---
+++ src/assets/FamousGraphs/PetersonGraph.gdf.ts
@@ -0,0 +1,29 @@
+const petersonGraph = `nodedef>name VARCHAR
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+edgedef>node1 VARCHAR,node2 VARCHAR
+a,b
+b,c
+c,d
+d,e
+e,a
+a,f
+b,g
+c,h
+d,i
+e,j
+f,h
+h,j
+j,g
+g,i
+i,f`;
+
+export default petersonGraph;
