4c424412c83bd0271e1e599ec9adecd65731a546
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-06-09 22:27:50 +0200

    Add test util function for generating undirected edges

--- testUtil/generationFromSimplerDatastructure.ts
+++ testUtil/generationFromSimplerDatastructure.ts
@@ -1,9 +1,16 @@
 import DirectedEdge from '../src/models/Edge/DirectedEdge/DirectedEdge';
 import NamedVertex from '../src/models/Vertex/NamedVertex/NamedVertex';
+import UndirectedEdge from '../src/models/Edge/UndirectedEdge/UndirectedEdge';
 
 export function generateDirectedEdgesWithNameVertex(definition: string[][]): DirectedEdge<NamedVertex>[] {
 	return definition.map(
 		([vertexNameA, vertexNameB]) => new DirectedEdge(new NamedVertex(vertexNameA), new NamedVertex(vertexNameB)),
+	);
+}
+
+export function generateUndirectedEdgesWithNameVertex(definition: string[][]): UndirectedEdge<NamedVertex>[] {
+	return definition.map(
+		([vertexNameA, vertexNameB]) => new UndirectedEdge(new NamedVertex(vertexNameA), new NamedVertex(vertexNameB)),
 	);
 }
 
