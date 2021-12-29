b15bbfd1b9659f50c5465e5787a7c3c69636eaf5
TimothyGillespie <timothy@gillespie.eu>
2021-12-29 15:33:15 +0100

    Add stryker configuration

---
+++ stryker.conf.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+	packageManager: 'npm',
+	reporters: ['html', 'clear-text', 'progress'],
+	testRunner: 'jest',
+	coverageAnalysis: 'perTest',
+	ignorePatterns: ['lib']
+};
