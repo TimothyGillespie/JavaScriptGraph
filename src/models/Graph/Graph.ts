898904297538022f4afa9d48fb202a8a848d4645
TimothyGillespie <timothy@gillespie.eu>
2022-02-09 01:17:52 +0100

    Add JSDocs on Graph object (not complete)

--- src/models/Graph/Graph.ts
+++ src/models/Graph/Graph.ts
@@ -9,6 +9,11 @@ import { TarjanStronglyConnectedComponentsAlgorithm } from './algorithms';
 import { MutableHashMap } from '@tgillespie/hash-data-structures';
 import { MutableHashSet } from '@tgillespie/hash-data-structures/lib/lib/mutable-hash-set/mutable-hash-set';
 
+/**
+ * The Graph class serves as a container for the different components and algorithms on the graph.
+ * It models itself redundantly to provide the best suitable and efficient data structure for various algorithms and the
+ * end user who can fetch the deep clone of the underlying data structures.
+ */
 export class Graph<V extends Vertex, E extends Edge<V>> {
 	// Redundant information storage for performance
 	protected _listOfEdges: E[];
@@ -20,6 +25,13 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 
 	protected _addUnknownVerticesInEdges: boolean;
 
+	/**
+	 * Creates an empty Graph object.
+	 *
+	 * @param addUnknownVerticesInEdges Whether vertices contained in an added Edge should be automatically added to the list
+	 * of vertices. If true it will add unknown vertices. If false it will throw an VertexNotFoundError when trying to add edges with unknown
+	 * vertices.
+	 */
 	constructor(addUnknownVerticesInEdges: boolean = false) {
 		this._listOfEdges = [];
 		this._listOfVertices = [];
@@ -29,6 +41,13 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		this._addUnknownVerticesInEdges = addUnknownVerticesInEdges;
 	}
 
+
+	/**
+	 * Attempt to add one or many vertices. Vertices which are already contained in the Graph (determined by their .equals() method)
+	 * are not ignored.
+	 *
+	 * @param vertex The vertex / vertices to be added. Duplicates are only processed once.
+	 */
 	addVertex(...vertex: V[]): Graph<V, E> {
 		const uniqueVertices = new MutableHashSet<V>();
 		const setOfGraphVertices = new MutableHashSet<V>();
@@ -46,6 +65,12 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		return this;
 	}
 
+	/**
+	 * Attempt to remove one or many vertices from the graph. Vertices which are not contained in the Graph (determined by their .equals() method)
+	 * are not ignored. Edges containing the vertex are also removed.
+	 *
+	 * @param vertex The vertex / vertices to be removed. Duplicates are only processed once.
+	 */
 	deleteVertex(...vertex: V[]): Graph<V, E> {
 		const uniqueVertices = new MutableHashSet<V>();
 		const setOfGraphVertices = new MutableHashSet<V>();
@@ -68,6 +93,15 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		return this;
 	}
 
+	/**
+	 * Attempt to add one or many edges. Edges which are already contained in the Graph (determined by their .equals() method)
+	 * are not ignored.
+	 *
+	 * @param edge The edge / edges to be added. Duplicates are only processed once.
+	 *
+	 * @throws VertexNotFoundError if addUnknownVerticesInEdges is set to true and any edges contains a vertex not contained
+	 * in the Graph. If this error is thrown none of the passed edges is added.
+	 */
 	addEdge(...edge: E[]): Graph<V, E> {
 		const uniqueEdges = new MutableHashSet<E>();
 		const setOfGraphEdges = new MutableHashSet<E>();
@@ -95,6 +129,12 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		return this;
 	}
 
+	/**
+	 * Attempt to remove one or many edges. Edges which are not contained in the Graph (determined by their .equals() method)
+	 * are not ignored.
+	 *
+	 * @param edge The edge / edges to be removed. Duplicates are only processed once.
+	 */
 	deleteEdge(...edge: E[]): Graph<V, E> {
 		const uniqueEdges = new MutableHashSet<E>();
 		const setOfGraphEdges = new MutableHashSet<E>();
@@ -116,36 +156,20 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		return this;
 	}
 
+	/**
+	 * Retrieves all vertices which are adjacent (either parent or child) of a given vertex.
+	 *
+	 * @param vertex The vertex which adjacent vertices should be found.
+	 */
 	getAdjacentVerticesFor(vertex: V): V[] {
 		return this.getAdjacencyList().getAdjacentVertices(vertex);
 	}
 
-	getAttachedEdges(vertex: V): E[] {
-		return this.getListOfEdges().filter(({vertexA, vertexB}) => {
-			return vertexA.equals(vertex) || vertexB.equals(vertex);
-		});
-	}
-
-	getOutgoingEdges(vertex: V): E[] {
-		return this.getListOfEdges().filter(({vertexA, vertexB, isDirected}) => {
-			if(isDirected()) {
-				return vertexA.equals(vertex)
-			}
-
-			return vertexA.equals(vertex) || vertexB.equals(vertex);
-		});
-	}
-
-	getIncomingEdges(vertex: V): E[] {
-		return this.getListOfEdges().filter(({vertexA, vertexB, isDirected}) => {
-			if(isDirected()) {
-				return vertexB.equals(vertex)
-			}
-
-			return vertexA.equals(vertex) || vertexB.equals(vertex);
-		});
-	}
-
+	/**
+	 * Retrieves all child vertices of a given vertex. Vertices attached via an undirected edges are always included.
+	 *
+	 * @param vertex The vertex which child vertices should be found.
+	 */
 	getChildVertices(vertex: V): V[] {
 		const allEdges = this.getListOfEdges();
 		const allVertices = this.getListOfVertices();
@@ -172,6 +196,55 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		return resultSet.toArray();
 	}
 
+	/**
+	 * Retrieves all edges attached to a vertex regardless of type and direction. Loop edges are also included.
+	 *
+	 * @param vertex The vertex which attached edges should be found.
+	 */
+	getAttachedEdges(vertex: V): E[] {
+		return this.getListOfEdges().filter(({vertexA, vertexB}) => {
+			return vertexA.equals(vertex) || vertexB.equals(vertex);
+		});
+	}
+
+	/**
+	 * Retrieves all outgoing vertices of a given vertex. If the edge is directed then this is the case when it is vertexA (source).
+	 * Undirected edges are always included. Loop edges are also included.
+	 *
+	 * @param vertex The vertex which attached edges should be found.
+	 */
+	getOutgoingEdges(vertex: V): E[] {
+		return this.getListOfEdges().filter(({vertexA, vertexB, isDirected}) => {
+			if(isDirected()) {
+				return vertexA.equals(vertex)
+			}
+
+			return vertexA.equals(vertex) || vertexB.equals(vertex);
+		});
+	}
+
+	/**
+	 * Retrieves all incoming vertices of a given vertex. If the edge is directed then this is the case when it is vertexB (target).
+	 * Undirected edges are always included. Loop edges are also included.
+	 *
+	 * @param vertex The vertex which attached edges should be found.
+	 */
+	getIncomingEdges(vertex: V): E[] {
+		return this.getListOfEdges().filter(({vertexA, vertexB, isDirected}) => {
+			if(isDirected()) {
+				return vertexB.equals(vertex)
+			}
+
+			return vertexA.equals(vertex) || vertexB.equals(vertex);
+		});
+	}
+
+	/**
+	 * Retrieves edges in the graph with the given source and target vertex. Undirected edges disregard source and target check both orders.
+	 *
+	 * @param vertexA The source vertex.
+	 * @param vertexB The target vertex.
+	 */
 	getEdges(vertexA: V, vertexB: V): E[] {
 		return this.getListOfEdges().filter(
 			(maybeWantedEdge) =>
@@ -182,6 +255,12 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		);
 	}
 
+	/**
+	 * Transposes all edges of the Graph by calling the transpose method on all edges. This inverts source and target vertex.
+	 * For directed edges this is supposed to inverse their direction. It should be without effect for undirected edges.
+	 *
+	 * References to the edges remain in-tact. The adjacency matrix is regenerated out-dating all references to the matrix.
+	 */
 	transpose() {
 		this._listOfEdges.forEach((singleEdge) => {
 			singleEdge.transpose();
@@ -268,6 +347,15 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		}
 	}
 
+	/**
+	 * Retrieves the subgraph which contains the given vertices. Edges missing a vertex as a result are not included.
+	 * Unlike other methods this one generates and returns an entirely new Graph and does not mutate the Graph it is called on.
+	 *
+	 * @param vertices The vertices to form the subgraph with.
+	 *
+	 * @throws VertexNotFoundError if at least on of the passed vertices is not actually contained in the Graph per its equals
+	 * method.
+	 */
 	getSubgraph(vertices: V[]): Graph<V, E> {
 		vertices.forEach((singleVertex) => this.validateVertexIsContainedInGraph(singleVertex));
 
@@ -289,39 +377,82 @@ export class Graph<V extends Vertex, E extends Edge<V>> {
 		return TarjanStronglyConnectedComponentsAlgorithm.getStronglyConnectedComponentFor(this);
 	}
 
+	/**
+	 * Returns a deep clone of the Graph. Including edges and vertices.
+	 */
 	copy(): this {
 		return cloneDeep(this);
 	}
 
+	/**
+	 * Retrieves a deep clone of the list of edges used to represent this graph internally.
+	 */
 	getListOfEdges(): E[] {
 		return cloneDeep(this._listOfEdges);
 	}
 
+	/**
+	 * Retrieves a deep clone of the list of vertices used to represent this graph internally.
+	 */
 	getListOfVertices(): V[] {
 		return cloneDeep(this._listOfVertices);
 	}
 
+	/**
+	 * Retrieves a deep clone of the adjacency matrix used to represent this graph internally.
+	 */
 	getAdjacencyMatrix(): AdjacencyMatrix<V> {
 		return cloneDeep(this._adjacencyMatrix);
 	}
 
+	/**
+	 * Retrieves deep clone of the adjacency list used to represent this graph internally.
+	 */
 	getAdjacencyList(): AdjacencyList<V> {
 		return cloneDeep(this._adjacencyList);
 	}
 
+	/**
+	 * Whether vertices contained in an added Edge should be automatically added to the list of vertices.
+	 *
+	 * If true it will add unknown vertices.
+	 * If false it will throw an VertexNotFoundError when trying to add edges with unknown vertices.
+	 */
 	get addUnknownVerticesInEdges(): boolean {
 		return this._addUnknownVerticesInEdges;
 	}
 
+	/**
+	 * Validates that the vertices of an edge are contained in Graph. If not it will throw a VertexNotFoundError.
+	 *
+	 * @param edge The edges which vertices should be checked to be contained in the Graph.
+	 *
+	 * @throws VertexNotFoundError if any vertex of the edge does not exist in the Graph.
+	 */
 	validateEdgeVerticesAreContainedInGraph(edge: E) {
 		this.validateVertexIsContainedInGraph(edge.vertexA);
 		this.validateVertexIsContainedInGraph(edge.vertexB);
 	}
 
+	/**
+	 * Validates that a given vertex is contained in Graph. If not it will throw a VertexNotFoundError.
+	 *
+	 * @param vertex The vertex which should be checked to be contained in the Graph.
+	 *
+	 * @throws VertexNotFoundError if the vertex does not exist in the Graph.
+	 */
 	validateVertexIsContainedInGraph(vertex: V) {
 		if (!this.isVertexContainedInGraph(vertex)) throw new VertexNotFoundError(vertex);
 	}
 
+	/**
+	 * Determines whether a given vertex is contained in Graph.
+	 *
+	 * true = is contained
+	 * false = is not contained
+	 *
+	 * @param vertex The vertex which should be checked to be contained in the Graph.
+	 */
 	isVertexContainedInGraph(vertex: V): boolean {
 		return this._listOfVertices.find((singleVertex) => singleVertex.equals(vertex)) !== undefined;
 	}
